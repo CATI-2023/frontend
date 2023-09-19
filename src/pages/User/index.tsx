03f0da40d5fe1ca49ad3fa2ac15107831e78a63f
brennokayan <brennokayan@hotmail.com>
2023-09-19 14:25:46 -0400

    create feat home page

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,12 +1,8 @@
-import { useNavigate } from "react-router-dom";
-import { logout } from "../../utils/Auth";
-
 export function UserPage() {
-    const navigate = useNavigate();
+
   return (
     <>
-      <h1>Vc está logado</h1>
-      <button onClick={() => {logout(), navigate("/")}}>Logout</button>
+      <p>mexendo aqui</p>
     </>
   );
 }
