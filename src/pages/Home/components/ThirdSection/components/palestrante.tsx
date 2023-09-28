6e866375f0cc5ac1adf971f77383b6c217271cd0
Frank Willian Rodrigues da Silva <frank.wrs@hotmail.com>
2023-09-28 09:32:22 -0400

    fix conflicts

--- src/pages/Home/components/ThirdSection/components/palestrante.tsx
+++ src/pages/Home/components/ThirdSection/components/palestrante.tsx
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import { Avatar, Box } from "@mui/material";
-import "../style.css"
-export function Palestrante() {
-  return (
-    <>
-      <Box
-        display={"flex"}
-        alignItems={"center"}
-        flexDirection={"column"}
-        mt={"4em"}
-        width={"100%"}
-      >
-        <Avatar sx={{ height: "8em", width: "8em" }} />
-        <p className="name-palestrante-third-section"> Palestrante</p>
-        <Box px={{xs: "1.5rem", sm: "1.5rem", md: "1.5rem", lg: "3rem", xl: "3rem"}}>
-          <p className="text-palestrante-third-section">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
-        </Box>
-=======
 import { Box } from "@mui/material";
 
 type Props = {
@@ -45,7 +21,6 @@ export function Palestrante({ nome, atuacao, tema, lattes, foto }: Props) {
         <p className="text-palestrante-third-section">
           <a href={lattes}>Currículum Lattes</a>
         </p>
->>>>>>> fix_input_palestrantes_dados
       </Box>
     </>
   );
