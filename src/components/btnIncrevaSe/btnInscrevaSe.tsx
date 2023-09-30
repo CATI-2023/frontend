9e327d16a16d34d8f3bc45afd9bf392b2e0f61c6
brennokayan <brennokayan@hotmail.com>
2023-09-29 22:05:28 -0400

    fix-segunda secao, remocao do link para inscrição na Faesp

--- src/components/btnIncrevaSe/btnInscrevaSe.tsx
+++ src/components/btnIncrevaSe/btnInscrevaSe.tsx
@@ -2,7 +2,8 @@ import "./style.css";
 export function ButtonInscrevaSe() {
   return (
     <>
-      <a href="https://eva.faespe.org.br/xivcati/">
+      {/* <a href="https://eva.faespe.org.br/xivcati/"> */}
+      <a href="#">
         <button className="button-subscribe-second-section">
           INSCREVA-SE AGORA
         </button>
