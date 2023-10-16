4d33e18a0f53adb58e37cba26a1eb4504cb07a50
brennokayan <brennokayan@hotmail.com>
2023-10-16 11:52:10 -0400

    feat-apoiadores: criada tela para ver, criar, editar e deletar

--- src/pages/orgPages/Apoiadores/index.tsx
+++ src/pages/orgPages/Apoiadores/index.tsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import { Navbar } from "../../../components/navbar/Navbar";
+import { apoiadores } from "../../../Types/type";
+import { getApoiadores } from "../../../services/apoiadores";
+import { ListaApoiadores } from "./components/listApoiadores";
+import { Container } from "@mui/material";
 
 export function OrganizacaoApoiadoresPage() {
+  const [apoiadores, setApoiadores] = useState<apoiadores |null>(null);
+  async function getPatrocinadores() {
+    getApoiadores()
+      .then((res) => setApoiadores(res))
+      .catch((err) => console.log(err));
+  }
+  useEffect(() => {
+    getPatrocinadores();
+  }, []);
   return (
     <>
-      <Navbar title="Apoiadores" typeUser="Organização"  />
-      Apoiadores
+      <Navbar title="Apoiadores" typeUser="Organização" />
+      <Container maxWidth="lg">
+        <ListaApoiadores apoiadores={apoiadores} />
+      </Container>
     </>
   );
 }
